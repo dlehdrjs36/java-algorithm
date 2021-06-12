8b527840204cba856eb6595a522983dbdd56a3b0
dglee <slsldkf123@naver.com>
2021-06-12 13:41:16 +0900

    [자료구조] 연결리스트기반 스택 추가

---
+++ src/datastructure/LinkedListStack.java
@@ -0,0 +1,90 @@
+package datastructure;
+
+public class LinkedListStack<T> {
+
+	//노드를 표현한 클래스
+	private class Node{
+		private T data;
+		private Node nextLink;
+		
+		private Node() {
+			this.data = null;
+			this.nextLink = null;
+		}
+		private Node(T data) {
+			this.data = data;
+			this.nextLink = null;
+		}
+	}
+	
+	private Node head;
+	
+	public LinkedListStack() {
+		head = null;
+	}
+
+	/**
+	 * 스택이 비었는지 확인
+	 * @return
+	 */
+	public boolean SIsEmpty() {
+		if(head == null)
+			return true;	
+		
+		return false;
+	}
+	
+	/**
+	 * 스택의 push 연산
+	 * @param data
+	 */
+	public void SPush(T data) {
+		Node newNode = new Node(data);
+		newNode.nextLink = head;
+		head = newNode;
+	}
+	
+	/**
+	 * 스택의 pop 연산
+	 * @return
+	 */
+	public T SPop() {
+		if(SIsEmpty())
+			new ArrayIndexOutOfBoundsException("Stack Memory Error!");
+		
+		T rdata = head.data;
+		head = head.nextLink;
+		return rdata;
+	}
+	
+	/**
+	 * 스택의 peek 연산
+	 * @return
+	 */
+	public T SPeek() {
+		if(SIsEmpty())
+			new ArrayIndexOutOfBoundsException("Stack Memory Error!");
+		
+		return head.data;
+	}
+	
+	public static void main(String[] args) {
+		// Stack의 생성 및 초기화 ///////
+		LinkedListStack<Integer> stack = new LinkedListStack<>();
+
+		// 데이터 넣기 ///////
+		stack.SPush(1);
+		stack.SPush(2);
+		stack.SPush(3);
+		stack.SPush(4);
+		stack.SPush(5);
+
+		// 데이터 조회 ///////
+		System.out.println(stack.SPeek());
+		
+		// 데이터 꺼내기 ///////
+		while(!stack.SIsEmpty())
+			System.out.printf("%d ", stack.SPop()); 
+		
+	}
+}
