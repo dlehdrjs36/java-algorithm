fa8f3e384d93bb002be8fd5f88879ab808df0cab
dglee <slsldkf123@naver.com>
2022-03-01 01:50:22 +0900

    [백준] 17299 문제 풀이 수정

--- src/backjun/Q_17299.java
+++ src/backjun/Q_17299.java
@@ -1,49 +1,57 @@
 package backjun;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
+/**
+ * 백준
+ * @see <a href="https://www.acmicpc.net/problem/17299">17299번 문제</a>
+ */
 public class Q_17299 {
-
-
-
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         Stack<Integer> stack = new Stack<>();
 
-
-
         try {
             String readLine = bufferedReader.readLine();
-            String sequence  = bufferedReader.readLine();
-            String[] nge = new String[Integer.parseInt(readLine)];
+            String sequence = bufferedReader.readLine();
             String[] split = sequence.split(" ");
+            int[] nge = new int[Integer.parseInt(readLine)];
             //수열에서 나온 횟수 파악
-            int[] freq = new int[Integer.parseInt(readLine)];
-            for (int i = 0; i < split.length; i++) {
-                freq[Integer.parseInt(split[i])] += 1;
+            int[] freq = new int[1000001];
+
+            for (String s : split) {
+                freq[Integer.parseInt(s)] += 1;
             }
 
-            for (int i = 0; i < split.length; i++) {
-                while(!stack.empty() && freq[stack.peek()] < freq[Integer.parseInt(split[i])]) {
-                    nge[stack.peek()] = split[i]; //오큰수
+            stack.push(0); //숫자가 하나만 있는 경우
+            for (int i = 1; i < split.length; i++) {
+                if(stack.empty()) {
+                    stack.push(i);
+                }
+                while(!stack.empty() && freq[Integer.parseInt(split[stack.peek()])] < freq[Integer.parseInt(split[i])]) {
+                    nge[stack.peek()] = Integer.parseInt(split[i]); //오큰수
                     stack.pop();
                 }
                 stack.push(i);
             }
-            while (!stack.empty()) {
-                nge[stack.peek()] = "-1";
-                stack.pop();
 
+            while(!stack.empty()) {
+                nge[stack.peek()] = -1;
+                stack.pop();
             }
+
             for (int j = 0; j < nge.length; j++) {
-                if(j == nge.length -1)
-                    bufferedWriter.write(nge[j]);
-                else
-                    bufferedWriter.write(nge[j]+" ");
+                String gubun = j == nge.length-1 ? "" : " ";
+                bufferedWriter.write(nge[j]+gubun);
             }
             bufferedWriter.flush();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
