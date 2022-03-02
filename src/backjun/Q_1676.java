ce788d0fd33fad1c1a3096d0659bcf430697baf3
dglee <slsldkf123@naver.com>
2022-03-03 04:29:55 +0900

    [백준] 1676 문제 풀이 추가

--- src/backjun/Q_1676.java
+++ src/backjun/Q_1676.java
@@ -13,7 +13,8 @@ public class Q_1676 {
 
         int num = Integer.parseInt(br.readLine());
         int sum = 0;
-
+        //0은 2x5일 때 발생한다. 일반적으로 5의 개수가 적으므로 5의 개수만 검사한다.
+        //정확하게 하려면 2의 개수도 검사하여 5의 개수와 일치하는만큼 0의 개수가 결정된다.
         for (int i = 5; i <= num; i *= 5) {
             sum += num/i;
         }
