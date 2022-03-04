d08e6fd1813892acbf3563414583027eb9ea4a8e
dglee <slsldkf123@naver.com>
2022-03-04 23:59:39 +0900

    [백준] 9613 문제 풀이 추가

---
+++ src/backjun/Q_9613.java
@@ -0,0 +1,38 @@
+package backjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 백준
+ * @see <a href="https://www.acmicpc.net/problem/9613">9613번 문제</a>
+ */
+public class Q_9613 {
+    public static int gcd(int a, int b) {
+        if(b == 0) {
+            return a;
+        } else {
+            return gcd(b, a%b);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+        while(count-- > 0) {
+            String readLine = br.readLine();
+            String[] split = readLine.split(" ");
+            int numCount = Integer.parseInt(split[0]);
+            int sum = 0;
+
+            for (int i = 1; i < split.length; i++) {
+                for (int j = i + 1; j < split.length; j++) {
+                    sum += gcd(Integer.parseInt(split[i]), Integer.parseInt(split[j]));
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
